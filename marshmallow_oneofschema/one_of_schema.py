e9d1652c67f3a417b49ceaf2d295d6c75be2a44a
Dylan Rajguru <dylan.rajguru@gmail.com>
2019-10-15 13:31:33 -0700

    Passing session parameter through for compatibility with marshmallow sqlalchemy

--- marshmallow_oneofschema/one_of_schema.py
+++ marshmallow_oneofschema/one_of_schema.py
@@ -109,7 +109,7 @@ class OneOfSchema(Schema):
             result[self.type_field] = obj_type
         return result
 
-    def load(self, data, *, many=None, partial=None, unknown=None):
+    def load(self, data, *, session=None, many=None, partial=None, unknown=None):
         errors = {}
         result_data = []
         result_errors = {}
@@ -119,7 +119,7 @@ class OneOfSchema(Schema):
         if not many:
             try:
                 result = result_data = self._load(
-                    data, partial=partial, unknown=unknown
+                    data, session=session, partial=partial, unknown=unknown
                 )
                 #  result_data.append(result)
             except ValidationError as error:
@@ -128,7 +128,7 @@ class OneOfSchema(Schema):
         else:
             for idx, item in enumerate(data):
                 try:
-                    result = self._load(item, partial=partial)
+                    result = self._load(item, session=session, partial=partial)
                     result_data.append(result)
                 except ValidationError as error:
                     result_errors[idx] = error.normalized_messages()
@@ -143,7 +143,7 @@ class OneOfSchema(Schema):
             exc = ValidationError(errors, data=data, valid_data=result)
             raise exc
 
-    def _load(self, data, *, partial=None, unknown=None):
+    def _load(self, data, *, session=None, partial=None, unknown=None):
         if not isinstance(data, dict):
             raise ValidationError({"_schema": "Invalid data type: %s" % data})
 
@@ -172,6 +172,9 @@ class OneOfSchema(Schema):
         schema = type_schema if isinstance(type_schema, Schema) else type_schema()
 
         schema.context.update(getattr(self, "context", {}))
+
+        if session:
+            return schema.load(data, session=session, many=False, partial=partial, unknown=unknown)
 
         return schema.load(data, many=False, partial=partial, unknown=unknown)
 
